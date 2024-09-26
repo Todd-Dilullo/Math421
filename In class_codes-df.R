17dd7947638cb28a8da945e2185296cc4a59f889
Todd-Dilullo <tdilullo@bryant.edu>
2024-09-26 14:11:11 -0400

    A

---
+++ In class_codes-df.R
@@ -0,0 +1,11 @@
+#Read in a csv file
+df <-  read.csv('WHO-COVID-19-global-data.csv')
+df <-  read.csv('https://bryantstats.github.io/math421/data/WHO-COVID-19-global-data.csv')
+# show the nams of the column
+names(df)
+
+#show structure of the data in general
+str(df)
+
+
+
